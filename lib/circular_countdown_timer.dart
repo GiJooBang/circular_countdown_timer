4f7ca9401741a39a97dd38bbb0ace86ee5498524
GiJooBang <141035337+GiJooBang@users.noreply.github.com>
2023-08-20 23:32:29 +0900

    Update circular_countdown_timer.dart

--- lib/circular_countdown_timer.dart
+++ lib/circular_countdown_timer.dart
@@ -273,8 +273,9 @@ class CircularCountDownTimerState extends State<CircularCountDownTimer>
   }
 
   @override
-  /*Widget build(BuildContext context) {
-    return SizedBox(
+  Widget build(BuildContext context) {
+    return column(
+      SizedBox(
       width: widget.width,
       height: widget.height,
       child: AnimatedBuilder(
@@ -301,7 +302,7 @@ class CircularCountDownTimerState extends State<CircularCountDownTimer>
                             backgroundGradient: widget.backgroundGradient),
                       ),
                     ),
-                    widget.isTimerTextShown
+                    /*widget.isTimerTextShown
                         ? Align(
                             alignment: FractionalOffset.center,
                             //alignment: FractionalOffset.bottomCenter,
@@ -315,62 +316,30 @@ class CircularCountDownTimerState extends State<CircularCountDownTimer>
                               textAlign: widget.textAlign,
                             ),
                           )
-                        : Container(),
+                        : Container(),*/
                   ],
                 ),
               ),
             );
           }),
     );
-  }*/
-  Widget build(BuildContext context) {
-  return SizedBox(
-    width: widget.width,
-    height: widget.height,
-    child: AspectRatio(
-      aspectRatio: 1.0,
-      child: Stack(
-        children: <Widget>[
-          Positioned.fill(
-            child: CustomPaint(
-              painter: CustomTimerPainter(
-                animation: _countDownAnimation ?? _controller,
-                fillColor: widget.fillColor,
-                fillGradient: widget.fillGradient,
-                ringColor: widget.ringColor,
-                ringGradient: widget.ringGradient,
-                strokeWidth: widget.strokeWidth,
-                strokeCap: widget.strokeCap,
-                isReverse: widget.isReverse,
-                isReverseAnimation: widget.isReverseAnimation,
-                backgroundColor: widget.backgroundColor,
-                backgroundGradient: widget.backgroundGradient,
-              ),
-            ),
-          ),
-          Align(
-            alignment: FractionalOffset.center,
-            //alignment: FractionalOffset.bottomCenter,
-            child: AnimatedBuilder(
-              animation: _controller!,
-              builder: (context, child) {
-                return Text(
-                  time,
-                  style: widget.textStyle ??
-                      const TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.black,
-                      ),
-                  textAlign: widget.textAlign,
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
+    widget.isTimerTextShown
+                        ? Align(
+                            //alignment: FractionalOffset.center,
+                            alignment: FractionalOffset.bottomCenter,
+                            child: Text(
+                              time,
+                              style: widget.textStyle ??
+                                  const TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.black,
+                                  ),
+                              textAlign: widget.textAlign,
+                            ),
+                          )
+                        : Container(),
+    )}
+
 
   @override
   void dispose() {
