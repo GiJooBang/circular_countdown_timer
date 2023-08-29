aed653a91a81139c03521dd2fc0c749afd839c4e
GiJooBang <141035337+GiJooBang@users.noreply.github.com>
2023-08-29 23:57:36 +0900

    Update circular_countdown_timer.dart

--- lib/circular_countdown_timer.dart
+++ lib/circular_countdown_timer.dart
@@ -355,11 +355,6 @@ class CircularCountDownTimerState extends State<CircularCountDownTimer>
                     ),
                   ),
                 ),
-                //const SizedBox(
-                //  height: 10
-                //),
-                //if (widget.isTimerTextShown)
-                //  const SizedBox(height: 30), // Add spacing between CustomPaint and Text
                 if (widget.isTimerTextShown)
                   Text(
                     time,
